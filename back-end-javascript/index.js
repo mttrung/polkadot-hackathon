3bb14a64f88bf8f8bd11027cd714fe9ea50d7015
Trung Mai <trung@servishero.com>
2020-10-18 14:38:49 +0700

    Update back-end-javascript for using public provider

--- back-end-javascript/index.js
+++ back-end-javascript/index.js
@@ -1,19 +1,27 @@
-#!/usr/bin/env node
-const { ApiPromise } = require("@polkadot/api");
+const { ApiPromise, WsProvider } = require("@polkadot/api");
 
 async function main() {
+  const provider = new WsProvider('wss://rpc.polkadot.io');
+  const api = await ApiPromise.create({ provider });
+
   const params = process.argv.slice(2);
   if (params.length > 2) {
     throw Error("wrong number of arguments");
   }
 
-  const api = await ApiPromise.create();
   if (params.length == 1 && params[0] == 'get_latest_block') {
     const header = await api.rpc.chain.getHeader();
-    console.log(`Latest block number is ${header.number}`)
+    console.log(`Latest block number is ${header.number}`);
+
   } else if (params.length == 2 && params[0] == 'get_block') {
-    const block = await api.rpc.chain.getBlock(params[1]);
-    console.log(`Block ${params[1]}:`, JSON.stringify(block, null, 2));
+    if (Number.isInteger(Number(params[1]))) {
+      const hash = await api.rpc.chain.getBlockHash(params[1]);
+      const block = await api.rpc.chain.getBlock(hash);
+      console.log(`Block ${params[1]}:`, JSON.stringify(block, null, 2));
+    } else {
+      const block = await api.rpc.chain.getBlock(params[1]);
+      console.log(`Block ${params[1]}:`, JSON.stringify(block, null, 2));
+    }
   } else {
     throw Error("Invalid arguments")
   }
